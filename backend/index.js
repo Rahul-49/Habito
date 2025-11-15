c4cb9f079c432b073c93ce960b3b422e55806047
Rahul-49 <141146333+Rahul-49@users.noreply.github.com>
2025-11-15 18:53:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,14 @@ const allowedOrigins = [
   "http://localhost:5173",
   process.env.FRONTEND_URL
 ];
-
-const app = express();
-app.use(express.json());
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
+
+const app = express();
+app.use(express.json());
+
 
 
 app.use(
